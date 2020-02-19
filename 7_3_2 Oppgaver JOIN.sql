0dc60dcf38e303cd22d708051b392ee0f593cde6
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-02-19 15:57:54 +0100

    Oppgave 4-8 JOIN

--- 7_3_2 Oppgaver JOIN.sql
+++ 7_3_2 Oppgaver JOIN.sql
@@ -10,7 +10,7 @@ where city.name like 'O%o'
 order by city.Population desc;
 
 select *
-from city;
+from country;
 
 /*3*/
 select Name, Continent
@@ -24,13 +24,35 @@ group by city.CountryCode
 order by Cities desc;
 
 /*5*/
-select country.Name, country.Population, country.Continent, city.Name, city.population as 'PopulationCity'
+select country.Name, country.Population, country.Continent, city.Name, city.Population as 'PopulationCity'
 from country 
 join city on country.Code = city.CountryCode
 where city.population < 1000
 order by country.Name;
 
 /*6*/
+select country.Name as 'Country', country.GovernmentForm, city.Name as 'City' , city.Population
+from city
+left join country on country.Code = city.CountryCode
+where country.GovernmentForm like '%onarchy%'
+order by city.Population desc;
+
+/*7*/
+select city.Name, country.Continent, city.Population
+from city 
+left join country on  country.Code = city.CountryCode
+where city.Population > 8000000
+order by  city.Population desc;
+
+/*8*/
+select country.Name, country.Continent, count(country.Name) as 'Languages'
+from country
+left join countryLanguage on country.Code = countryLanguage.CountryCode
+group by country.Name
+having country.Continent = 'Asia' and Languages >= 10;
+
+
+
 
 
 
