dd9d537f423a73a16d514999b523ba470d57bae8
Oystein-FS <oystein.synnestvedt@gmail.com>
2020-02-19 15:19:05 +0100

    Oppgave 3-5 JOIN

--- 7_3_2 Oppgaver JOIN.sql
+++ 7_3_2 Oppgaver JOIN.sql
@@ -10,4 +10,28 @@ where city.name like 'O%o'
 order by city.Population desc;
 
 select *
-from country;
+from city;
+
+/*3*/
+select Name, Continent
+from country
+order by Continent, Name;
+
+/*4*/
+select country.Name, count(city.CountryCode) as 'Cities', avg(city.Population) as 'AvgPopulation'
+from city left join country on city.CountryCode = country.Code
+group by city.CountryCode
+order by Cities desc;
+
+/*5*/
+select country.Name, country.Population, country.Continent, city.Name, city.population as 'PopulationCity'
+from country 
+join city on country.Code = city.CountryCode
+where city.population < 1000
+order by country.Name;
+
+/*6*/
+
+
+
+
